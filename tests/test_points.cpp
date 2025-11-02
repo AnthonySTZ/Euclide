2d584d1be7ebc0d299c64ea95d8f32ccc0e5c79d
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-02 13:40:57 +0100

    test(AddPointGetIndex): test if the addPoint meghot dreturns the correct newly created point

--- tests/test_points.cpp
+++ tests/test_points.cpp
@@ -16,5 +16,14 @@ TEST(Points, AddPoint){
     EXPECT_EQ(p.posY[0], 1.0);
     EXPECT_EQ(p.posZ[0], -3.0);
 }
+
+TEST(Points, AddPointGetIndex){
+    Points p;
+    size_t first_index = p.addPoint(5.0, 1.0, -3.0);
+    size_t second_index = p.addPoint(5.0, 1.0, 8.0);
+    EXPECT_EQ(p.size(), 2);
+    EXPECT_EQ(first_index, 0);
+    EXPECT_EQ(second_index, 1);
+}
     
 }
