768fdb8ef88063162cec0773ad1421406df10754
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-02 16:07:50 +0100

    doc(Node::setInput)

--- src/core/nodes/node.cpp
+++ src/core/nodes/node.cpp
@@ -2,6 +2,13 @@
 
 namespace butter {
 
+/**
+ * @brief Create a node connection between two nodes
+ * 
+ * @param t_index The current input index to be connected from a source node
+ * @param t_sourceNode The source node
+ * @param t_sourceIndex The output index of the source node
+ */
 void Node::setInput(const size_t t_index, const std::weak_ptr<Node> &t_sourceNode, const size_t t_sourceIndex)
 {
     if(t_index >= m_inputConnections.size()) return;
