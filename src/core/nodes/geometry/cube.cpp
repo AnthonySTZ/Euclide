9c336cc495fdfece91687bb3400de1feef551784
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-04 13:34:59 +0100

    feat(Cube): create cube

--- src/core/nodes/geometry/cube.cpp
+++ src/core/nodes/geometry/cube.cpp
@@ -6,7 +6,31 @@ std::shared_ptr<Mesh> Cube::compute(const size_t t_index, const std::vector<std:
 {
     auto output = std::make_shared<Mesh>();
 
+    py::vec3 position = getField<Float3Field>("position")->getValue();
+    py::vec3 size = getField<Float3Field>("size")->getValue();
+
+    size *= 0.5;
+    /* Top Face */
+    size_t p0 = output->addPoint(size[0] + position[0], size[1] + position[1], -size[2] + position[3]);
+    size_t p1 = output->addPoint(-size[0] + position[0], size[1] + position[1], -size[2] + position[3]);
+    size_t p2 = output->addPoint(-size[0] + position[0], size[1] + position[1], size[2] + position[3]);
+    size_t p3 = output->addPoint(size[0] + position[0], size[1] + position[1], size[2] + position[3]);
     
+    /* Bottom Face */
+    size_t p4 = output->addPoint(size[0] + position[0], -size[1] + position[1], -size[2] + position[3]);
+    size_t p5 = output->addPoint(-size[0] + position[0], -size[1] + position[1], -size[2] + position[3]);
+    size_t p6 = output->addPoint(-size[0] + position[0], -size[1] + position[1], size[2] + position[3]);
+    size_t p7 = output->addPoint(size[0] + position[0], -size[1] + position[1], size[2] + position[3]);
+
+    /* Clockwise order */
+    output->addPrimitive({0, 1, 2, 3}); // Top face
+    output->addPrimitive({7, 6, 5, 4}); // Bottom face
+
+    output->addPrimitive({4, 5, 1, 0});
+    output->addPrimitive({3, 2, 6, 7});
+
+    output->addPrimitive({0, 3, 7, 4});
+    output->addPrimitive({1, 5, 6, 2});
 
     return output;
 }
--- tests/nodes/test_cube.cpp
+++ tests/nodes/test_cube.cpp
@@ -6,10 +6,30 @@
 namespace butter {
 
 TEST(Cube, TestCubeName) {
-
     auto cube = std::make_shared<Cube>();
     EXPECT_EQ(cube->name(), "Cube");
+}
 
+TEST(Cube, CubeOutput) {
+    auto cube = std::make_shared<Cube>();
+
+    // Set fields
+    cube->getField<Float3Field>("position")->setValue(0.0f, 0.0f, 0.0f);
+    cube->getField<Float3Field>("size")->setValue(2.0f, 2.0f, 2.0f);
+
+    auto result = cube->cook(0);
+
+    // Check points
+    ASSERT_EQ(result->points.size(), 8);
+    EXPECT_FLOAT_EQ(result->points.posX[0], 1.0f);
+    EXPECT_FLOAT_EQ(result->points.posY[0], 1.0f);
+    EXPECT_FLOAT_EQ(result->points.posZ[0], -1.0f);
+
+    EXPECT_FLOAT_EQ(result->points.posX[6], -1.0f);
+    EXPECT_FLOAT_EQ(result->points.posY[6], -1.0f);
+    EXPECT_FLOAT_EQ(result->points.posZ[6], 1.0f);
+
+    ASSERT_EQ(result->primitives.size(), 6);
 }
 
 }
