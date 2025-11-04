b55e40c54576960caa2d7326f68c7da74b334d4d
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-04 11:49:36 +0100

    style(Cube): move compute function of cube in cpp file

--- src/core/nodes/geometry/cube.cpp
+++ src/core/nodes/geometry/cube.cpp
@@ -1,1 +1,14 @@
 #include "cube.h"
+
+namespace butter {
+    
+std::shared_ptr<Mesh> Cube::compute(const size_t t_index, const std::vector<std::shared_ptr<Mesh>> &t_inputs)
+{
+    auto output = std::make_shared<Mesh>();
+
+    
+
+    return output;
+}
+
+}
--- src/core/nodes/geometry/cube.h
+++ src/core/nodes/geometry/cube.h
@@ -10,10 +10,7 @@ public:
     Cube() : Node(0, 1, "Cube") {}
 
 private:
-    std::shared_ptr<Mesh> compute(const size_t t_index, const std::vector<std::shared_ptr<Mesh>>& t_inputs) override {
-        auto output = std::make_shared<Mesh>();
-        return output;
-    }
+    std::shared_ptr<Mesh> compute(const size_t t_index, const std::vector<std::shared_ptr<Mesh>>& t_inputs) override;
 
 };
         
