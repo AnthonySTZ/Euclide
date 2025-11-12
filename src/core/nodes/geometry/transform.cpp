6191171db384523229b034c6f9629fb8c7875770
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-12 17:58:29 +0100

    fix(Transform): fix mistype attribute

--- src/core/nodes/geometry/transform.cpp
+++ src/core/nodes/geometry/transform.cpp
@@ -7,7 +7,7 @@ std::shared_ptr<Mesh> Transform::compute(const size_t t_index, const std::vector
     if (t_inputs[0] == nullptr) return std::make_shared<Mesh>();
     auto output = std::make_shared<Mesh>(*t_inputs[0]);
 
-    vec3 position = getField<Float3Field>("position")->getValue();
+    vec3 position = getField<Float3Field>("translate")->getValue();
     vec3 size = getField<Float3Field>("size")->getValue();
     vec3 rotate = getField<Float3Field>("rotate")->getValue();
 
