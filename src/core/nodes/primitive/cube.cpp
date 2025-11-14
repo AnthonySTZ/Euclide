19dc42d48d8b32f3a42477c9ee51e2daa1ede64b
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-14 14:56:51 +0100

    style(Cube): move constructor in cpp

--- src/core/nodes/primitive/cube.cpp
+++ src/core/nodes/primitive/cube.cpp
@@ -1,7 +1,29 @@
 #include "cube.h"
 
 namespace butter {
+
+Cube::Cube()
+    : Node(0, 1, "Cube") 
+{
+    auto positionField = std::make_shared<Float3Field>(0.0, 0.0, 0.0);
+    positionField->setMetadata(NodeFieldMetadata{
+        displayName: "Position",
+        min: -10.0f,
+        max: 10.0f,
+        step: 0.02f
+    });
+    addField("position", positionField);
     
+    auto sizeField = std::make_shared<Float3Field>(1.0, 1.0, 1.0);
+    sizeField->setMetadata(NodeFieldMetadata{
+        displayName: "Size",
+        min: 0.0f,
+        max: 10.0f,
+        step: 0.02f
+    });
+    addField("size", sizeField);
+}
+
 std::shared_ptr<Mesh> Cube::compute(const size_t t_index, const std::vector<std::shared_ptr<Mesh>> &t_inputs)
 {
     auto output = std::make_shared<Mesh>();
--- src/core/nodes/primitive/cube.h
+++ src/core/nodes/primitive/cube.h
@@ -9,25 +9,7 @@ namespace butter {
 class Cube: public Node {
     
 public:
-    Cube() : Node(0, 1, "Cube") {
-        auto positionField = std::make_shared<Float3Field>(0.0, 0.0, 0.0);
-        positionField->setMetadata(NodeFieldMetadata{
-            displayName: "Position",
-            min: -10.0f,
-            max: 10.0f,
-            step: 0.02f
-        });
-        addField("position", positionField);
-        
-        auto sizeField = std::make_shared<Float3Field>(1.0, 1.0, 1.0);
-        sizeField->setMetadata(NodeFieldMetadata{
-            displayName: "Size",
-            min: 0.0f,
-            max: 10.0f,
-            step: 0.02f
-        });
-        addField("size", sizeField);
-    }
+    Cube();
 
 private:
     std::shared_ptr<Mesh> compute(const size_t t_index, const std::vector<std::shared_ptr<Mesh>>& t_inputs) override;
