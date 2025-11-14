4b4709326b0d3419121b72d0b831a7bc5f951aab
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-14 19:50:30 +0100

    feat(Transform): add metadata to fields

--- src/core/nodes/geometry/transform.cpp
+++ src/core/nodes/geometry/transform.cpp
@@ -2,15 +2,47 @@
 
 namespace butter {
     
+Transform::Transform()
+    : Node(1, 1, "Transform")
+{
+    auto translateField = std::make_shared<Float3Field>(0.0, 0.0, 0.0);
+    translateField->setMetadata(NodeFieldMetadata{
+        displayName: "Translate",
+        step: 0.02f
+    });
+    addField("translate", translateField);
+    
+    auto sizeField = std::make_shared<Float3Field>(1.0, 1.0, 1.0);
+    sizeField->setMetadata(NodeFieldMetadata{
+        displayName: "Size",
+        step: 0.02f
+    });
+    addField("size", sizeField);
+
+    auto rotateField = std::make_shared<Float3Field>(0.0, 0.0, 0.0);
+    rotateField->setMetadata(NodeFieldMetadata{
+        displayName: "Rotate",
+        step: 0.02f
+    });
+    addField("rotate", rotateField);
+}
+
 std::shared_ptr<Mesh> Transform::compute(const size_t t_index, const std::vector<std::shared_ptr<Mesh>> &t_inputs)
 {
-    //TODO:
     if (t_inputs[0] == nullptr) return std::make_shared<Mesh>();
     auto output = std::make_shared<Mesh>(*t_inputs[0]);
 
-    float3 position = getField<Float3Field>("translate")->getValue();
+    float3 translate = getField<Float3Field>("translate")->getValue();
     float3 size = getField<Float3Field>("size")->getValue();
     float3 rotate = getField<Float3Field>("rotate")->getValue();
+
+    auto& points = output->points;
+
+    for (size_t i = 0; i < points.size(); ++i) {
+        points.posX[i] *= size[0];
+        points.posY[i] *= size[1];
+        points.posZ[i] *= size[2];
+    }
 
     return output;
 }
--- src/core/nodes/geometry/transform.h
+++ src/core/nodes/geometry/transform.h
@@ -9,11 +9,7 @@ namespace butter {
 class Transform: public Node {
     
 public:
-    Transform() : Node(1, 1, "Transform") {
-        addField("translate", std::make_shared<Float3Field>(0.0, 0.0, 0.0));
-        addField("size", std::make_shared<Float3Field>(1.0, 1.0, 1.0));
-        addField("rotate", std::make_shared<Float3Field>(0.0, 0.0, 0.0));
-    }
+    Transform();
 
 private:
     std::shared_ptr<Mesh> compute(const size_t t_index, const std::vector<std::shared_ptr<Mesh>>& t_inputs) override;
