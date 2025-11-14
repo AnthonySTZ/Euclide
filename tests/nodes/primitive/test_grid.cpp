67635353cd5d33cfcc4286874f02bf79109df786
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-14 17:55:06 +0100

    test(Grid): GeneratesCorrectPrimitiveCount

--- tests/nodes/primitive/test_grid.cpp
+++ tests/nodes/primitive/test_grid.cpp
@@ -16,4 +16,16 @@ TEST(Grid, GeneratesCorrectPointCount) {
     EXPECT_EQ(mesh->points.size(), 12);
 }
 
+TEST(Grid, GeneratesCorrectPrimitiveCount)
+{
+    auto grid = std::make_shared<Grid>();
+
+    // 4×5 divisions → 20 quads
+    grid->getField<Int2Field>("divisions")->setValue(4, 5);
+
+    auto mesh = grid->cook(0);
+
+    EXPECT_EQ(mesh->primitives.size(), 20);
+}
+
 }
