14742ee5de729d2d636fbf77478d8cae95617296
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-14 18:14:15 +0100

    test(Grid): Grid2x3CustomPositionAndSize

--- tests/nodes/primitive/test_grid.cpp
+++ tests/nodes/primitive/test_grid.cpp
@@ -2,6 +2,8 @@
 #include "nodes/primitive/grid.h"
 
 #include "fields/int2field.h"
+#include "fields/float2field.h"
+#include "fields/float3field.h"
 
 #include <memory>
 
@@ -26,6 +28,52 @@ TEST(Grid, GeneratesCorrectPrimitiveCount)
     auto mesh = grid->cook(0);
 
     EXPECT_EQ(mesh->primitives.size(), 20);
+}
+
+TEST(Grid, CorrectSpacing)
+{
+    auto grid = std::make_shared<Grid>();
+
+    grid->getField<Float3Field>("position")->setValue(0, 0, 0);
+    grid->getField<Float2Field>("size")->setValue(4, 4);
+    grid->getField<Int2Field>("divisions")->setValue(2, 2);
+
+    auto mesh = grid->cook(0);
+
+    // Expected point grid:
+    // x: -2, 0, +2
+    // z: -2, 0, +2
+
+    ASSERT_EQ(mesh->points.size(), 9);
+
+    EXPECT_FLOAT_EQ(mesh->points.posX[0], -2);
+    EXPECT_FLOAT_EQ(mesh->points.posX[1], 0);
+    EXPECT_FLOAT_EQ(mesh->points.posX[2], 2);
+}
+
+TEST(Grid, Grid2x3CustomPositionAndSize) {
+
+    auto grid = std::make_shared<Grid>();
+
+    grid->getField<Float3Field>("position")->setValue(10.0, 5.0, 20.0);
+    grid->getField<Float2Field>("size")->setValue(4, 6);
+    grid->getField<Int2Field>("divisions")->setValue(2, 3);
+
+    auto mesh = grid->cook(0);
+    EXPECT_EQ(mesh->points.size(), 12);
+
+    float expectedX[3] = { 8.0f, 10.0f, 12.0f };
+    float expectedZ[4] = { 17.0f, 19.0f, 21.0f, 23.0f };
+
+    for (size_t row = 0; row < 4; ++row) {
+        for (size_t col = 0; col < 3; ++col){
+            const size_t pointIndex = row * 3 + col;
+            EXPECT_FLOAT_EQ(mesh->points.posX[pointIndex], expectedX[col]);
+            EXPECT_FLOAT_EQ(mesh->points.posY[pointIndex], 5.0f);  // center Y
+            EXPECT_FLOAT_EQ(mesh->points.posZ[pointIndex], expectedZ[row]);
+        }
+    }
+
 }
 
 }
