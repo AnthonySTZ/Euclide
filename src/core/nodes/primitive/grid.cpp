f5bc22c9bc33e55a7395bd523ee1ec30116110c5
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-14 17:29:30 +0100

    feat(Grid): create primitives

--- src/core/nodes/primitive/grid.cpp
+++ src/core/nodes/primitive/grid.cpp
@@ -41,21 +41,34 @@ std::shared_ptr<Mesh> Grid::compute(const size_t t_index, const std::vector<std:
     float2 size = getField<Float2Field>("size")->getValue();
     const int2 divisions = getField<Int2Field>("divisions")->getValue();
 
-    const int columns = divisions[0] + 1;
-    const int rows = divisions[1] + 1;
+    const int columns = divisions[0];
+    const int rows = divisions[1];
 
-    const float columnSpacing = size[0] / static_cast<float>(columns - 1);
-    const float rowSpacing = size[1] / static_cast<float>(rows - 1);
+    const int columnsPoints = columns + 1;
+    const int rowsPoints = rows + 1;
+
+    const float columnSpacing = size[0] / static_cast<float>(columns);
+    const float rowSpacing = size[1] / static_cast<float>(rows);
 
     const float startX = position[0] - (size[0] * 0.5f);
     const float startZ = position[2] - (size[1] * 0.5f);
 
-    output->points.reserve(rows * columns);
-    for (size_t row = 0; row < rows; ++row) {
-        for (size_t column = 0; column < columns; ++column) {
+    // Create Points
+    output->points.reserve(rowsPoints * columnsPoints);
+    for (size_t row = 0; row < rowsPoints; ++row) {
+        for (size_t column = 0; column < columnsPoints; ++column) {
             const float offsetX = static_cast<float>(column) * columnSpacing;
             const float offsetZ = static_cast<float>(row) * rowSpacing;
             output->addPoint(startX + offsetX, position[1], startZ + offsetZ);
+        }
+    }
+
+    // Create Primitives
+    for (uint32_t row = 0; row < rows; ++row) {
+        for (uint32_t column = 0; column < columns; ++column) {
+            const uint32_t top_left = row * (columnsPoints) + column;
+            const uint32_t bottom_left = top_left + columnsPoints;
+            output->addPrimitive({top_left, top_left + 1, bottom_left + 1, bottom_left});
         }
     }
 
