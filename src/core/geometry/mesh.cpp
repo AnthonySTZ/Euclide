47f9844b6f325e2354f60d239c1349db1fff3350
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-21 15:57:24 +0100

    test(mesh): write test for center

--- src/core/geometry/mesh.cpp
+++ src/core/geometry/mesh.cpp
@@ -10,42 +10,41 @@ namespace butter {
 
 float3 Mesh::center() const
 {
-    if (points.size() == 0) return {0.0, 0.0, 0.0};
-
-    Timer timer{"center"};
-
     float3 sumPos{0.0, 0.0, 0.0};
+    const size_t numPoints = points.size();
+    if (numPoints == 0) return sumPos;
+    
     size_t i = 0;
 
     #ifdef USE_SIMD
-
-        __m256 __sumX = _mm256_setzero_ps();
-        __m256 __sumY = _mm256_setzero_ps();
-        __m256 __sumZ = _mm256_setzero_ps();
-
-        for (; i + 8 <= points.size(); i += 8){
-            __m256 __posX = _mm256_load_ps(&points.posX[i]);
-            __m256 __posY = _mm256_load_ps(&points.posY[i]);
-            __m256 __posZ = _mm256_load_ps(&points.posZ[i]);
-
-            __sumX = _mm256_add_ps(__sumX, __posX);
-            __sumY = _mm256_add_ps(__sumY, __posY);
-            __sumZ = _mm256_add_ps(__sumZ, __posZ);
+        if (numPoints >= 8) {
+            __m256 __sumX = _mm256_setzero_ps();
+            __m256 __sumY = _mm256_setzero_ps();
+            __m256 __sumZ = _mm256_setzero_ps();
+            
+            for (; i + 8 <= numPoints; i += 8){
+                __m256 __posX = _mm256_load_ps(&points.posX[i]);
+                __m256 __posY = _mm256_load_ps(&points.posY[i]);
+                __m256 __posZ = _mm256_load_ps(&points.posZ[i]);
+                
+                __sumX = _mm256_add_ps(__sumX, __posX);
+                __sumY = _mm256_add_ps(__sumY, __posY);
+                __sumZ = _mm256_add_ps(__sumZ, __posZ);
+            }
+            
+            sumPos[0] = _mm256_hsum_ps(__sumX);
+            sumPos[1] = _mm256_hsum_ps(__sumY);
+            sumPos[2] = _mm256_hsum_ps(__sumZ);
         }
-
-        _mm256_sum_store_ss(&sumPos[0], __sumX);
-        _mm256_sum_store_ss(&sumPos[1], __sumY);
-        _mm256_sum_store_ss(&sumPos[2], __sumZ);
-
     #endif
 
-    for (; i < points.size(); ++i) {
+    for (; i < numPoints; ++i) {
         sumPos[0] += points.posX[i];
         sumPos[1] += points.posY[i];
         sumPos[2] += points.posZ[i];
     }
 
-    const float divisor = static_cast<float>(points.size());
+    const float divisor = static_cast<float>(numPoints);
 
     sumPos[0] /= divisor;
     sumPos[1] /= divisor;
--- tests/geometry/test_mesh.cpp
+++ tests/geometry/test_mesh.cpp
@@ -20,4 +20,58 @@ TEST(Mesh, CreateTriangle){
     EXPECT_EQ(mesh.vertices.size(), 3);
 }
 
+TEST(Mesh, GetMeshCenter){
+    Mesh mesh;
+
+    mesh.addPoint(1.0, 5.0, 2.0);
+    mesh.addPoint(2.0, 5.0, -8.0);
+    mesh.addPoint(-1.0, 5.0, 2.0);
+    mesh.addPoint(-18.0, 2.5, 1.0);
+    mesh.addPoint(0.0, -5.0, 2.0);
+    mesh.addPoint(-1.4, 9.0, -8.0);
+
+    float3 expected {
+        -2.9f, 3.58333f, -1.5f
+    };
+
+    float3 center = mesh.center();
+    float x = center[0];
+    float y = center[1];
+    float z = center[2];
+
+    EXPECT_NEAR(x, expected[0], 1e-2f);
+    EXPECT_NEAR(y, expected[1], 1e-2f);
+    EXPECT_NEAR(z, expected[2], 1e-2f);
+}
+
+TEST(Mesh, GetMeshCenterMoreThan8Points){
+    Mesh mesh;
+
+    mesh.addPoint(1.0, 5.0, 2.0);
+    mesh.addPoint(2.0, 5.0, -8.0);
+    mesh.addPoint(-1.0, 5.0, 2.0);
+    mesh.addPoint(-18.0, 2.5, 1.0);
+    mesh.addPoint(0.0, -5.0, 2.0);
+    mesh.addPoint(-1.4, 9.0, -8.0);
+    mesh.addPoint(1.0, 5.0, 2.0);
+    mesh.addPoint(2.0, 5.0, -8.0);
+    mesh.addPoint(-1.0, 5.0, 2.0);
+    mesh.addPoint(-18.0, 2.5, 1.0);
+    mesh.addPoint(0.0, -5.0, 2.0);
+    mesh.addPoint(-1.4, 9.0, -8.0);
+
+    float3 expected {
+        -2.9f, 3.58333f, -1.5f
+    };
+
+    float3 center = mesh.center();
+    float x = center[0];
+    float y = center[1];
+    float z = center[2];
+
+    EXPECT_NEAR(x, expected[0], 1e-2f);
+    EXPECT_NEAR(y, expected[1], 1e-2f);
+    EXPECT_NEAR(z, expected[2], 1e-2f);
+}
+
 }
