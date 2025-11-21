aa1a32794a386f195b4d39516dd2fcc08eaf8bf1
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-21 15:57:16 +0100

    fix(simd): pass __m256 by reference instead of value

--- src/core/utils/simd.h
+++ src/core/utils/simd.h
@@ -4,14 +4,14 @@
 
 namespace butter {
 
-inline void _mm256_sum_store_ss(float* __P, const __m256 __A) {
+inline float _mm256_hsum_ps(const __m256& __A) {
     __m128 lower = _mm256_castps256_ps128(__A); // __A = 8 floats to add -> split into two 128bits (4 floats), lower and higher
     __m128 higher = _mm256_extractf128_ps(__A, 1);
     __m128 sum_128 = _mm_add_ps(lower, higher); // add them into 4 floats
     sum_128 = _mm_hadd_ps(sum_128, sum_128); // horizontal add: [a, b, c, d] -> [a + b, c + d, a + b, c + d]
     sum_128 = _mm_hadd_ps(sum_128, sum_128); // horizontal add again: [(a + b) + (c + d), (a + b) + (c + d), (a + b) + (c + d), (a + b) + (c + d)]
                                     // which is equal to [sum, sum, sum, sum]
-    _mm_store_ss(__P, sum_128);
+    return _mm_cvtss_f32(sum_128);
 }
 
 }
