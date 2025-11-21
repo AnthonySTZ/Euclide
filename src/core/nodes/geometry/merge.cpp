76db33e3531b0606daf2d8f414d8d7c6321ec0e9
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-21 17:12:25 +0100

    feat(Merge): implement merge

--- src/core/nodes/geometry/merge.cpp
+++ src/core/nodes/geometry/merge.cpp
@@ -19,8 +19,59 @@ std::shared_ptr<Mesh> Merge::compute(const size_t t_index, const std::vector<std
     if (t_inputs[1] == nullptr) return std::make_shared<Mesh>(*t_inputs[0]);
 
     auto output = std::make_shared<Mesh>(*t_inputs[0]);
-    
     Timer timer{"Merge"};
+
+    const auto& meshToMerge = t_inputs[1];
+    
+    auto& outputPoints = output->points;
+    const size_t numPoints = outputPoints.size(); 
+    size_t pointIdx = numPoints;
+    
+    const auto& pointsToMerge = meshToMerge->points;
+    outputPoints.reserve(outputPoints.size() + pointsToMerge.size());
+    outputPoints.resize(outputPoints.size() + pointsToMerge.size());
+    for (size_t i = 0; i < pointsToMerge.size(); ++i) {
+        outputPoints.posX[pointIdx] = pointsToMerge.posX[i];
+        outputPoints.posY[pointIdx] = pointsToMerge.posY[i];
+        outputPoints.posZ[pointIdx] = pointsToMerge.posZ[i];
+        
+        outputPoints.normalX[pointIdx] = pointsToMerge.normalX[i];
+        outputPoints.normalY[pointIdx] = pointsToMerge.normalY[i];
+        outputPoints.normalZ[pointIdx] = pointsToMerge.normalZ[i];
+        
+        outputPoints.colorR[pointIdx] = pointsToMerge.colorR[i];
+        outputPoints.colorG[pointIdx] = pointsToMerge.colorG[i];
+        outputPoints.colorB[pointIdx] = pointsToMerge.colorB[i];
+        
+        pointIdx++;
+    }
+    
+    auto& outputVertices = output->vertices;
+    const size_t numVertices = outputVertices.size(); 
+    size_t vertexIdx = numVertices;
+    
+    const auto& verticesToMerge = meshToMerge->vertices;
+    outputVertices.reserve(outputVertices.size() + verticesToMerge.size());
+    outputVertices.resize(outputVertices.size() + verticesToMerge.size());
+    for (size_t i = 0; i < verticesToMerge.size(); ++i) {
+        Vertex vert = verticesToMerge[i];
+        vert.refPoint += numPoints;
+        outputVertices[vertexIdx] = vert;
+        vertexIdx++;
+    }
+
+    auto& outputPrimitives = output->primitives;
+    size_t primIdx = outputPrimitives.size();
+    
+    const auto& primsToMerge = meshToMerge->primitives;
+    outputPrimitives.reserve(outputPrimitives.size() + primsToMerge.size());
+    outputPrimitives.resize(outputPrimitives.size() + primsToMerge.size());
+    for (size_t i = 0; i < primsToMerge.size(); ++i) {
+        Primitive prim = primsToMerge[i];
+        prim.verticesIndex += numVertices;
+        outputPrimitives[primIdx] = prim;
+        primIdx++;
+    }
 
     return output;
 }
