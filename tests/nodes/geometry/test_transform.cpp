17fc5d9b033fb202a8c72a3b346df738209b5af4
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-24 11:43:40 +0100

    test(Transform): add TestScale

--- tests/nodes/geometry/test_transform.cpp
+++ tests/nodes/geometry/test_transform.cpp
@@ -30,4 +30,31 @@ TEST(Transform, TestTranslation) {
     EXPECT_EQ(mesh.points.posZ[p2], 2.5);
 }
 
+TEST(Transform, TestScale) {
+
+    Mesh mesh;
+
+    uint32_t p0 = mesh.addPoint({1.0, 0.0, 5.0});
+    uint32_t p1 = mesh.addPoint({-1.0, 9.0, -5.0});
+    uint32_t p2 = mesh.addPoint({0.0, 10.0, 0.0});
+
+    TransformSettings settings {
+        scale: {1.0, -5.0, 2.5}
+    };
+
+    Transform::transform(mesh, settings);
+
+    EXPECT_EQ(mesh.points.posX[p0], 1.0);
+    EXPECT_EQ(mesh.points.posY[p0], 0.0);
+    EXPECT_EQ(mesh.points.posZ[p0], 12.5);
+    
+    EXPECT_EQ(mesh.points.posX[p1], -1.0);
+    EXPECT_EQ(mesh.points.posY[p1], -45.0);
+    EXPECT_EQ(mesh.points.posZ[p1], -12.5);
+
+    EXPECT_EQ(mesh.points.posX[p2], 0.0);
+    EXPECT_EQ(mesh.points.posY[p2], -50.0);
+    EXPECT_EQ(mesh.points.posZ[p2], 0.0);
+}
+
 }
