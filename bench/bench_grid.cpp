e289381a9fe58a36e49933dfd2e4b350fe3c5e13
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-25 11:08:29 +0100

    bench(Grid): change units

--- bench/bench_grid.cpp
+++ bench/bench_grid.cpp
@@ -2,20 +2,14 @@
 
 #include "nodes/primitive/grid.h"
 
-#include <numeric>
-
 static void BM_Grid1000x1000(benchmark::State& state) {
     butter::GridSettings settings{
         .divisions = {1000, 1000}
     };
     for (auto _ : state) {
-        state.PauseTiming();
-        butter::Mesh mesh{};
-        state.ResumeTiming();
-        
+        butter::Mesh mesh;        
         butter::Grid::createGrid(mesh, settings);
-        benchmark::DoNotOptimize(mesh);
     }
 }
 
-BENCHMARK(BM_Grid1000x1000);
+BENCHMARK(BM_Grid1000x1000)->Unit(benchmark::kMillisecond);
