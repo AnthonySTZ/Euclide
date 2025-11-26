2d1282062b54110ec6c6b6e33e1191b71ec04d7c
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-26 17:45:27 +0100

    style(Halfedge): remove tabspace

--- src/core/geometry/halfedge.h
+++ src/core/geometry/halfedge.h
@@ -3,12 +3,12 @@
 namespace butter {
 
 struct HalfEdge {
-        static constexpr uint32_t NO_TWIN = UINT32_MAX;
-        
-        uint32_t next; // Next HalfEdge Index
-        uint32_t origin; // Origin vertex Index
-        uint32_t face; // Prim Index
-        uint32_t twin = NO_TWIN; // Twin HalfEdge Index
+    static constexpr uint32_t NO_TWIN = UINT32_MAX;
+
+    uint32_t next; // Next HalfEdge Index
+    uint32_t origin; // Origin vertex Index
+    uint32_t face; // Prim Index
+    uint32_t twin = NO_TWIN; // Twin HalfEdge Index
 };
 
 }
