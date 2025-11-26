c80accc26a8a6afdebcf71ef9026aa78a7853dab
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-26 17:07:36 +0100

    bench-Mesh): Bench Halfedges construction

---
+++ bench/bench_mesh.cpp
@@ -0,0 +1,18 @@
+#include <benchmark/benchmark.h>
+
+#include "nodes/primitive/grid.h"
+
+static void BM_HalfedgeConstructionGrid1000x1000(benchmark::State& state) { // 1400ms
+    butter::Mesh mesh;
+    butter::GridSettings settings{
+        .divisions = {1000, 1000}
+    };
+    butter::Grid::createGrid(mesh, settings);
+
+    for (auto _ : state) {
+        std::vector<butter::HalfEdge> edges = mesh.computeHalfEdges();
+        benchmark::DoNotOptimize(edges);
+    }
+}
+
+BENCHMARK(BM_HalfedgeConstructionGrid1000x1000)->Iterations(10)->Unit(benchmark::kMillisecond);
