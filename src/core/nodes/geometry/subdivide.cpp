e192929d2254eaca951ff8176f50e128497af9d1
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-28 15:53:42 +0100

    feat(Subdive): compute facepoints

--- src/core/nodes/geometry/subdivide.cpp
+++ src/core/nodes/geometry/subdivide.cpp
@@ -41,7 +41,12 @@ void Subdivide::subdivide(Mesh &t_mesh, const SubdivideSettings& t_settings)
     auto& primitives = t_mesh.primitives;
     auto& vertices = t_mesh.vertices;
     
-    const std::vector<HalfEdge> halfEdges_d = t_mesh.computeHalfEdges();    
+    const std::vector<HalfEdge> halfEdges_d = t_mesh.computeHalfEdges();
+    size_t numOfEdges = 0;
+    for (size_t h = 0; h < halfEdges_d.size(); ++h) {
+        const HalfEdge& hd = halfEdges_d[h];
+        if (hd.twin > h) numOfEdges++;
+    }
 
     // Halfedge refinement
     std::vector<HalfEdge> halfEdges_d1(halfEdges_d.size() * 4); // Next division
@@ -52,6 +57,19 @@ void Subdivide::subdivide(Mesh &t_mesh, const SubdivideSettings& t_settings)
         halfEdges_d1[newFaceId + 1].twin = 4 * hd.next + 2;
         halfEdges_d1[newFaceId + 2].twin = 4 * hd.prev + 1;
         halfEdges_d1[newFaceId + 3].twin = hd.twin == HalfEdge::NO_TWIN ? HalfEdge::NO_TWIN : 4 * halfEdges_d[hd.twin].prev;
+    }
+
+    // Face points
+    size_t numOfPoints = points.size();
+    points.reserve(points.size() + primitives.size() + numOfEdges);
+    points.resize(points.size() + primitives.size() + numOfEdges);
+    for (size_t h = 0; h < halfEdges_d.size(); ++h) {
+        const HalfEdge& hd = halfEdges_d[h];
+        const float factor = 1.0f / static_cast<float>(primitives[hd.face].numVertices);
+        const uint32_t i = numOfPoints + hd.face;
+        points.posX[i] += points.posX[hd.origin] * factor; 
+        points.posY[i] += points.posY[hd.origin] * factor; 
+        points.posZ[i] += points.posZ[hd.origin] * factor; 
     }
 }
 
