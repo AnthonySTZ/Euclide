3a7b82c91f1b1c0ee6b689ab5bccc5542c09b6d6
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-30 16:12:44 +0100

    doc(imgui_utils)

--- src/app/interface/utils/imgui_utils.h
+++ src/app/interface/utils/imgui_utils.h
@@ -10,7 +10,9 @@
 #include <iostream>
 #include <string>
 
-inline ImVec2 operator+(const ImVec2 &lvec, const ImVec2 &rvec) {
+/// @brief ImGui 2D vector arithmetic operators for convenience.
+
+inline ImVec2 operator+(const ImVec2& lvec, const ImVec2& rvec) {
     return ImVec2(lvec.x + rvec.x, lvec.y + rvec.y);
 }
 
@@ -20,7 +22,7 @@ inline ImVec2& operator+=(ImVec2& lvec, const ImVec2& rvec) {
     return lvec;
 }
 
-inline ImVec2 operator-(const ImVec2 &lvec, const ImVec2 &rvec) {
+inline ImVec2 operator-(const ImVec2& lvec, const ImVec2& rvec) {
     return ImVec2(lvec.x - rvec.x, lvec.y - rvec.y);
 }
 
@@ -30,36 +32,49 @@ inline ImVec2& operator-=(ImVec2& lvec, const ImVec2& rvec) {
     return lvec;
 }
 
-inline ImVec2 operator*(const ImVec2 &lvec, const float &rval) {
+inline ImVec2 operator*(const ImVec2& lvec, const float& rval) {
     return ImVec2(lvec.x * rval, lvec.y * rval);
 }
 
-inline ImVec4 operator*(const ImVec4 &lvec, const float &rval) {
+inline ImVec4 operator*(const ImVec4& lvec, const float& rval) {
     return ImVec4(lvec.x * rval, lvec.y * rval, lvec.z * rval, lvec.w);
 }
 
-inline bool operator==(const ImVec2 &lvec, const ImVec2 &rvec) {
+inline bool operator==(const ImVec2& lvec, const ImVec2& rvec) {
     return (lvec.x == rvec.x) && (lvec.y == rvec.y);
 }
 
-inline bool operator!=(const ImVec2 &lvec, const ImVec2 &rvec) {
+inline bool operator!=(const ImVec2& lvec, const ImVec2& rvec) {
     return !(lvec == rvec);
 }
 
-static int StringImGuiCallBack(ImGuiInputTextCallbackData *data) {
-    /* Used to resize the string for a imGui input text*/
+/// @brief Callback to automatically resize a std::string for ImGui InputText widgets.
+/// @param data Pointer to ImGui callback data.
+/// @return Always returns 0.
+static int StringImGuiCallBack(ImGuiInputTextCallbackData* data) {
     if (data->EventFlag == ImGuiInputTextFlags_CallbackResize) {
-        std::string *str = (std::string *)data->UserData;
+        std::string* str = (std::string*)data->UserData;
         str->resize(data->BufTextLen);
-        data->Buf = (char *)str->c_str();
+        data->Buf = (char*)str->c_str();
     }
     return 0;
 }
 
+/// @brief Determines if three points are arranged in counter-clockwise order.
+/// @param A First point.
+/// @param B Second point.
+/// @param C Third point.
+/// @return True if points are counter-clockwise, false otherwise.
 static bool ccw(ImVec2 A, ImVec2 B, ImVec2 C) {
-	return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x);
+    return (C.y - A.y) * (B.x - A.x) > (B.y - A.y) * (C.x - A.x);
 }
 
+/// @brief Determines if two line segments intersect.
+/// @param A Start of first segment.
+/// @param B End of first segment.
+/// @param C Start of second segment.
+/// @param D End of second segment.
+/// @return True if the line segments AB and CD intersect.
 static bool lineIntersect(ImVec2 A, ImVec2 B, ImVec2 C, ImVec2 D) {
-	return ccw(A, C, D) != ccw(B, C, D) && ccw(A, B, C) != ccw(A, B, D);
+    return ccw(A, C, D) != ccw(B, C, D) && ccw(A, B, C) != ccw(A, B, D);
 }
