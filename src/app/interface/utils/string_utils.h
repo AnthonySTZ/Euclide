84a4aa108b7a43e8b7a84e6f467d1645781248b6
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-30 16:13:59 +0100

    doc(string_utils)

--- src/app/interface/utils/string_utils.h
+++ src/app/interface/utils/string_utils.h
@@ -2,16 +2,22 @@
 
 #include <string>
 #include <fstream>
+#include <vector>
 #include <algorithm>
+#include <stdexcept>
 
+/// @brief Convert a string to lowercase.
+/// @param t_string Input string.
+/// @return Lowercase version of the input string.
 inline std::string toLower(std::string t_string) {
     std::transform(t_string.begin(), t_string.end(), t_string.begin(), [](unsigned char c) { return std::tolower(c); });
     return t_string;
 }
 
 /// @brief Read the entire contents of a file into a string.
-/// @param t_filepath Path to the file.
-/// @return File contents as a string.
+/// @param t_filepath Path to the file to read.
+/// @return Contents of the file as a std::string.
+/// @throws std::runtime_error if the file cannot be opened.
 inline std::string readFile(const std::string& t_filepath) {
     std::ifstream file{t_filepath, std::ios::ate | std::ios::binary};
     if (!file.is_open())
@@ -24,5 +30,5 @@ inline std::string readFile(const std::string& t_filepath) {
     file.read(buffer.data(), fileSize);
     file.close();
 
-    return std::string(begin(buffer), end(buffer));
+    return std::string(buffer.begin(), buffer.end());
 }
