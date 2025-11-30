ed3aecceb3d433dbf067cc859d1552774461ee79
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-30 16:21:57 +0100

    style(main)

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,14 +1,12 @@
-#include <iostream>
-
 #include "interface/engine.h"
 
-int main(){
+int main() {
     butter::Engine engine{1600, 900, "Test"};
-    
+
     engine.interface().addNodeGraph();
     engine.interface().addViewport();
     engine.interface().addParameters();
-    
+
     engine.mainLoop();
     return 0;
 }
