11550898be76919410f7ac7339dcd3c358b16143
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-30 13:22:35 +0100

    doc(CuttingLine)

--- src/app/interface/panels/graph/cutting_line.cpp
+++ src/app/interface/panels/graph/cutting_line.cpp
@@ -1,11 +1,12 @@
 #include "cutting_line.h"
 
 namespace butter {
-    
+
 void CuttingLine::draw() const noexcept
 {
-    ImDrawList* drawList = ImGui::GetWindowDrawList();
-	drawList->AddLine(m_start, m_end, s_color, s_thickness);
+    if (ImDrawList* drawList = ImGui::GetWindowDrawList()) {
+        drawList->AddLine(m_start, m_end, COLOR, THICKNESS);
+    }
 }
 
-}
+} // namespace butter
--- src/app/interface/panels/graph/cutting_line.h
+++ src/app/interface/panels/graph/cutting_line.h
@@ -4,19 +4,26 @@
 
 namespace butter {
 
+/// @brief Represents a simple line to be drawn in the ImGui context.
 class CuttingLine {
 
 public:
-    CuttingLine(const ImVec2& t_start, const ImVec2& t_end) : m_start{t_start}, m_end{t_end} {}
+    /// @brief Construct a cutting line from start to end points.
+    /// @param t_start Starting position of the line.
+    /// @param t_end Ending position of the line.
+    CuttingLine(const ImVec2& t_start, const ImVec2& t_end) 
+        : m_start{t_start}, m_end{t_end} {}
 
+    /// @brief Draw the line in the current ImGui window.
+    /// @note This function assumes that an ImGui window is active.
     void draw() const noexcept;
 
 private:
-    ImVec2 m_start;
-    ImVec2 m_end;
-    const static constexpr ImU32 s_color = IM_COL32(255, 50, 50, 255);
-    const static constexpr float s_thickness = 1.5f;
+    ImVec2 m_start; ///< Start point of the line
+    ImVec2 m_end;   ///< End point of the line
 
+    static constexpr ImU32 COLOR = IM_COL32(255, 50, 50, 255); ///< Line color
+    static constexpr float THICKNESS = 1.5f;                   ///< Line thickness
 };
 
-}
+} // namespace butter
