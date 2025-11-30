7c192d6f914cdbb0ddaeb2d15883e711634d3e6b
AnthonySTZ <anthonycandy2309@gmail.com>
2025-11-30 16:56:20 +0100

    docd(math)

--- src/core/utils/math.h
+++ src/core/utils/math.h
@@ -4,15 +4,20 @@
 
 namespace butter {
 
-inline double radians(const double t_degrees) noexcept
-{
+/// @brief Converts an angle from degrees to radians.
+///
+/// @param t_degrees Angle in degrees.
+/// @return Angle in radians.
+inline double radians(const double t_degrees) noexcept {
     return t_degrees * M_PI / 180.0;
 }
 
-inline double degrees(const double t_radians) noexcept
-{
+/// @brief Converts an angle from radians to degrees.
+///
+/// @param t_radians Angle in radians.
+/// @return Angle in degrees.
+inline double degrees(const double t_radians) noexcept {
     return t_radians * 180.0 * M_1_PI;
 }
 
-
-}
+} // namespace butter
