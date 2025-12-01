f2802ed18f0d0e2a79187a1bc573c31fafa1dc15
AnthonySTZ <anthonycandy2309@gmail.com>
2025-12-01 18:15:44 +0100

    docs(ParametersRenderer)

--- src/app/interface/panels/parameters/parameters_renderer.h
+++ src/app/interface/panels/parameters/parameters_renderer.h
@@ -7,12 +7,22 @@ namespace euclide {
 class ParametersRenderer {
   public:
     ParametersRenderer(const std::weak_ptr<Parameters> t_parameters);
+
+    /// @brief Draw Parameters with correct style
     void draw();
 
   private:
+    /// @brief Draw parameters for the current selected node
     void drawParameters();
+
+    /// @brief Draw the name of the selected node
+    /// @param t_title
     void drawTitleName(const std::string& t_title);
+
+    /// @brief Push ImGui Parameters style
     void pushStyle();
+
+    /// @brief Pop ImGui Parameters style
     void popStyle();
 
   private:
