e8e9a05d925d0a42dfb8b8a7ce5191954d9b8e13
AnthonySTZ <anthonycandy2309@gmail.com>
2025-12-01 16:38:03 +0100

    feat(Viewport): init viewport

--- src/app/interface/panels/viewport/viewport.cpp
+++ src/app/interface/panels/viewport/viewport.cpp
@@ -1,1 +1,17 @@
 #include "viewport.h"
+
+#include "nodes/primitive/grid.h"
+
+namespace butter {
+
+Viewport::Viewport(const std::weak_ptr<Scene> t_scene) : m_scene(t_scene) {
+    Mesh grid{};
+    Grid::createGrid(grid, GridSettings{.position = {0.0, 0.0, 0.0}, .size = {10.0, 10.0}, .divisions = {10, 10}});
+    m_gridModel.updateWithMesh(grid);
+
+    if (auto scene = m_scene.lock()) {
+        scene->onMeshUpdate.subscribe([this](std::shared_ptr<Mesh> t_mesh) { m_renderModel.updateWithMesh(*t_mesh); });
+    }
+}
+
+} // namespace butter
--- src/app/interface/panels/viewport/viewport.h
+++ src/app/interface/panels/viewport/viewport.h
@@ -2,15 +2,22 @@
 
 #include "nodes/scene.h"
 
+#include "rendering/camera.h"
+#include "rendering/render_model.h"
+
 namespace butter {
 
 class Viewport {
   public:
-    Viewport(const std::weak_ptr<Scene> t_scene) : m_scene(t_scene) {}
+    Viewport(const std::weak_ptr<Scene> t_scene);
     ~Viewport() = default;
 
   private:
     std::weak_ptr<Scene> m_scene;
+
+    RenderModel m_renderModel; ///< Main scene render model.
+    RenderModel m_gridModel;
+    Camera m_camera;
 };
 
 } // namespace butter
