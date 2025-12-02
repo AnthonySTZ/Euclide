a951923bdea270a13aa11eb9fefa3ec4fec99816
AnthonySTZ <anthonycandy2309@gmail.com>
2025-12-02 20:44:14 +0100

    feat(GPU): find Queue family index

--- src/core/gpu/gpu_device.cpp
+++ src/core/gpu/gpu_device.cpp
@@ -8,6 +8,22 @@ GPUDevice::GPUDevice(VkInstance& t_vkInstance) : vkInstance(t_vkInstance) {
     pickPhysicalDevice();
 }
 
+std::optional<uint32_t> GPUDevice::findQueueFamilies(const VkPhysicalDevice& t_physicalDevice,
+                                                     const VkQueueFlagBits t_flags) const {
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(t_physicalDevice, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(t_physicalDevice, &queueFamilyCount, queueFamilies.data());
+
+    auto propIt = std::find_if(queueFamilies.begin(), queueFamilies.end(),
+                               [&](const VkQueueFamilyProperties& t_prop) { return t_prop.queueFlags & t_flags; });
+    if (propIt == queueFamilies.end())
+        return std::nullopt;
+
+    return std::distance(queueFamilies.begin(), propIt);
+}
+
 void GPUDevice::pickPhysicalDevice() {
     uint32_t deviceCount = 0;
     vkEnumeratePhysicalDevices(vkInstance, &deviceCount, nullptr);
--- src/core/gpu/gpu_device.h
+++ src/core/gpu/gpu_device.h
@@ -2,6 +2,8 @@
 
 #include <vulkan/vulkan.hpp>
 
+#include <optional>
+
 namespace euclide {
 
 class GPUDevice {
@@ -9,15 +11,37 @@ class GPUDevice {
     GPUDevice(VkInstance& t_vkInstance);
     ~GPUDevice() = default;
 
+    /// @brief Returns the used physical device
+    /// @return `VkPhysicalDevice` choosen based on its properties
     [[nodiscard]] VkPhysicalDevice physicalDevice() const noexcept { return m_physicalDevice; }
+
+    /// @brief Logical device, the 'interface' with the physical device
+    /// @return `VkDevice` Logical device
+    [[nodiscard]] VkDevice device() const noexcept { return m_device; }
+
+    /// @brief Find a queue family index that match the given flags
+    /// @param t_physicalDevice
+    /// @param t_flags
+    /// @return The Queue family index if found, any otherwise.
+    [[nodiscard]] std::optional<uint32_t> findQueueFamilies(const VkPhysicalDevice& t_physicalDevice,
+                                                            const VkQueueFlagBits t_flags) const;
+
+    /// @brief Find a queue family index that match the given flags using the current Physical Device
+    /// @param t_flags
+    /// @return The Queue family index if found, any otherwise.
+    [[nodiscard]] std::optional<uint32_t> findPhysicalQueueFamilies(const VkQueueFlagBits t_flags) const {
+        return findQueueFamilies(m_physicalDevice, t_flags);
+    }
 
   private:
     void pickPhysicalDevice();
 
   private:
-    VkInstance vkInstance;
+    VkInstance vkInstance = VK_NULL_HANDLE;
     VkPhysicalDevice m_physicalDevice = VK_NULL_HANDLE;
-    VkPhysicalDeviceProperties m_physicalDeviceProperties;
+    VkPhysicalDeviceProperties m_physicalDeviceProperties{};
+
+    VkDevice m_device = VK_NULL_HANDLE;
 };
 
 } // namespace euclide
