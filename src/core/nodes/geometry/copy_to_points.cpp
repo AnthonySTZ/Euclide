92e89edac4fb825b50732ddbf64201220e7b3d14
AnthonySTZ <anthonycandy2309@gmail.com>
2025-12-09 16:33:30 +0100

    feat(CopyToPoints): copy prim and vertices

--- src/core/nodes/geometry/copy_to_points.cpp
+++ src/core/nodes/geometry/copy_to_points.cpp
@@ -28,12 +28,13 @@ void CopyToPoints::copyToPoints(Mesh& t_mesh, const Mesh& t_points) {
     const auto points_to_copy = t_mesh.points;
     auto& new_points = t_mesh.points;
     const auto& points = t_points.points;
+    const size_t numOfDuplicates = points.size();
 
     const size_t pointsSize = points_to_copy.size();
     new_points.clear();
-    new_points.resize(pointsSize * points.size());
+    new_points.resize(pointsSize * numOfDuplicates);
 
-    for (size_t i = 0; i < points.size(); ++i) {
+    for (size_t i = 0; i < numOfDuplicates; ++i) {
         const float offsetX = points.posX[i];
         const float offsetY = points.posY[i];
         const float offsetZ = points.posZ[i];
@@ -52,6 +53,29 @@ void CopyToPoints::copyToPoints(Mesh& t_mesh, const Mesh& t_points) {
             new_points.colorR[pointIdx] = points_to_copy.colorR[j];
             new_points.colorG[pointIdx] = points_to_copy.colorG[j];
             new_points.colorB[pointIdx] = points_to_copy.colorB[j];
+        }
+    }
+
+    auto& vertices = t_mesh.vertices;
+    const size_t verticesSize = vertices.size();
+
+    vertices.resize(verticesSize + verticesSize * numOfDuplicates);
+    for (size_t i = 1; i <= numOfDuplicates; ++i) {
+        const size_t vertOffset = verticesSize * i;
+        for (size_t j = 0; j < verticesSize; ++j) {
+            vertices[vertOffset + j] = vertices[j];
+            vertices[vertOffset + j].refPoint += pointsSize * i;
+        }
+    }
+
+    auto& primitives = t_mesh.primitives;
+    const size_t primitivesSize = primitives.size();
+    primitives.resize(primitivesSize + primitivesSize * numOfDuplicates);
+    for (size_t i = 1; i <= numOfDuplicates; ++i) {
+        const size_t primOffset = primitivesSize * i;
+        for (size_t j = 0; j < primitivesSize; ++j) {
+            primitives[primOffset + j] = primitives[j];
+            primitives[primOffset + j].verticesIndex += verticesSize * i;
         }
     }
 }
