984e71875895969336d02bc27af00968c0660a08
AnthonySTZ <anthonycandy2309@gmail.com>
2025-12-12 11:17:22 +0100

    test(attributes): test resize

--- tests/geometry/test_attributes.cpp
+++ tests/geometry/test_attributes.cpp
@@ -3,12 +3,26 @@
 
 namespace euclide {
 
-TEST(Attributes, CreateAttribute) {
+TEST(Attributes, InitAttribute) {
     Attribute attr{"P", 3, AttributeType::ATTR_TYPE_FLOAT};
 
     EXPECT_EQ(attr.name, "P");
     EXPECT_EQ(attr.attrSize, 3);
     EXPECT_EQ(attr.getType(), AttributeType::ATTR_TYPE_FLOAT);
+}
+
+TEST(Attributes, ResizeFloat) {
+    Attribute attr{"P", 3, AttributeType::ATTR_TYPE_FLOAT};
+
+    attr.resize(5);
+
+    EXPECT_EQ(attr.size, 5);
+    for (size_t component = 0; component < 3; ++component) {
+        float* compData = static_cast<float*>(attr.data[component]);
+        for (size_t i = 0; i < 5; ++i) {
+            EXPECT_EQ(compData[i], 0.0f);
+        }
+    }
 }
 
 } // namespace euclide
