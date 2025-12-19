4616d8d5b87f8b3ea062193381cd7ae4a518eb11
AnthonySTZ <anthonycandy2309@gmail.com>
2025-12-19 15:10:24 +0100

    feat(perlin): implement

--- src/core/nodes/attributes/attribute_noise.cpp
+++ src/core/nodes/attributes/attribute_noise.cpp
@@ -2,6 +2,31 @@
 
 namespace euclide {
 
+const std::array<int, 512> AttributeNoise::perlinPermutations = {
+    151, 160, 137, 91,  90,  15,  131, 13,  201, 95,  96,  53,  194, 233, 7,   225, 140, 36,  103, 30,  69,  142, 8,
+    99,  37,  240, 21,  10,  23,  190, 6,   148, 247, 120, 234, 75,  0,   26,  197, 62,  94,  252, 219, 203, 117, 35,
+    11,  32,  57,  177, 33,  88,  237, 149, 56,  87,  174, 20,  125, 136, 171, 168, 68,  175, 74,  165, 71,  134, 139,
+    48,  27,  166, 77,  146, 158, 231, 83,  111, 229, 122, 60,  211, 133, 230, 220, 105, 92,  41,  55,  46,  245, 40,
+    244, 102, 143, 54,  65,  25,  63,  161, 1,   216, 80,  73,  209, 76,  132, 187, 208, 89,  18,  169, 200, 196, 135,
+    130, 116, 188, 159, 86,  164, 100, 109, 198, 173, 186, 3,   64,  52,  217, 226, 250, 124, 123, 5,   202, 38,  147,
+    118, 126, 255, 82,  85,  212, 207, 206, 59,  227, 47,  16,  58,  17,  182, 189, 28,  42,  223, 183, 170, 213, 119,
+    248, 152, 2,   44,  154, 163, 70,  221, 153, 101, 155, 167, 43,  172, 9,   129, 22,  39,  253, 19,  98,  108, 110,
+    79,  113, 224, 232, 178, 185, 112, 104, 218, 246, 97,  228, 251, 34,  242, 193, 238, 210, 144, 12,  191, 179, 162,
+    241, 81,  51,  145, 235, 249, 14,  239, 107, 49,  192, 214, 31,  181, 199, 106, 157, 184, 84,  204, 176, 115, 121,
+    50,  45,  127, 4,   150, 254, 138, 236, 205, 93,  222, 114, 67,  29,  24,  72,  243, 141, 128, 195, 78,  66,  215,
+    61,  156, 180, 151, 160, 137, 91,  90,  15,  131, 13,  201, 95,  96,  53,  194, 233, 7,   225, 140, 36,  103, 30,
+    69,  142, 8,   99,  37,  240, 21,  10,  23,  190, 6,   148, 247, 120, 234, 75,  0,   26,  197, 62,  94,  252, 219,
+    203, 117, 35,  11,  32,  57,  177, 33,  88,  237, 149, 56,  87,  174, 20,  125, 136, 171, 168, 68,  175, 74,  165,
+    71,  134, 139, 48,  27,  166, 77,  146, 158, 231, 83,  111, 229, 122, 60,  211, 133, 230, 220, 105, 92,  41,  55,
+    46,  245, 40,  244, 102, 143, 54,  65,  25,  63,  161, 1,   216, 80,  73,  209, 76,  132, 187, 208, 89,  18,  169,
+    200, 196, 135, 130, 116, 188, 159, 86,  164, 100, 109, 198, 173, 186, 3,   64,  52,  217, 226, 250, 124, 123, 5,
+    202, 38,  147, 118, 126, 255, 82,  85,  212, 207, 206, 59,  227, 47,  16,  58,  17,  182, 189, 28,  42,  223, 183,
+    170, 213, 119, 248, 152, 2,   44,  154, 163, 70,  221, 153, 101, 155, 167, 43,  172, 9,   129, 22,  39,  253, 19,
+    98,  108, 110, 79,  113, 224, 232, 178, 185, 112, 104, 218, 246, 97,  228, 251, 34,  242, 193, 238, 210, 144, 12,
+    191, 179, 162, 241, 81,  51,  145, 235, 249, 14,  239, 107, 49,  192, 214, 31,  181, 199, 106, 157, 184, 84,  204,
+    176, 115, 121, 50,  45,  127, 4,   150, 254, 138, 236, 205, 93,  222, 114, 67,  29,  24,  72,  243, 141, 128, 195,
+    78,  66,  215, 61,  156, 180};
+
 AttributeNoise::AttributeNoise() : Node(1, 1, "AttrNoise") {
     auto kindField = std::make_shared<NodeField<int>>(0);
     kindField->setMetadata(NodeFieldMetadata{
@@ -15,6 +40,15 @@ AttributeNoise::AttributeNoise() : Node(1, 1, "AttrNoise") {
     attributeField->setMetadata(NodeFieldMetadata{displayName : "Attribute Name"});
     addField("attributeName", attributeField);
 
+    auto sizeField = std::make_shared<NodeField<int>>(0);
+    sizeField->setMetadata(NodeFieldMetadata{
+        displayName : "Attribute Size",
+        min : 0,
+        max : 4,
+        step : 1,
+    });
+    addField("size", sizeField);
+
     auto noiseField = std::make_shared<NodeField<int>>(0);
     noiseField->setMetadata(NodeFieldMetadata{
         displayName : "Noise",
@@ -22,6 +56,15 @@ AttributeNoise::AttributeNoise() : Node(1, 1, "AttrNoise") {
         choices : std::move(std::vector<std::string>{"Perlin"})
     });
     addField("noise", noiseField);
+
+    auto octavesField = std::make_shared<NodeField<int>>(1);
+    octavesField->setMetadata(NodeFieldMetadata{
+        displayName : "Octaves",
+        min : 1,
+        step : 1,
+    });
+    addField("octaves", octavesField);
+    // TODO: add frequency float3 and persistence
 }
 
 std::shared_ptr<Mesh> AttributeNoise::compute(const size_t t_index,
@@ -31,7 +74,22 @@ std::shared_ptr<Mesh> AttributeNoise::compute(const size_t t_index,
 
     auto output = std::make_shared<Mesh>(*t_inputs[0]);
 
-    perlinNoise(*output, output->pointAttribs, "Cd", 3, 1, 1);
+    const int attrSize = getField<NodeField<int>>("size")->getValue();
+    if (attrSize < 1)
+        return output;
+
+    const Kind kind = static_cast<Kind>(getField<NodeField<int>>("kind")->getValue());
+    const std::string attrName = getField<NodeField<std::string>>("attributeName")->getValue();
+    const int octaves = getField<NodeField<int>>("octaves")->getValue();
+
+    if (attrName.empty())
+        return output;
+
+    if (kind == Kind::POINTS) {
+        perlinNoise(*output, output->pointAttribs, attrName, attrSize, octaves, 1);
+    } else if (kind == Kind::PRIMITIVES) {
+        perlinNoise(*output, output->primAttribs, attrName, attrSize, octaves, 1);
+    }
 
     return output;
 }
@@ -55,11 +113,15 @@ void AttributeNoise::perlinNoise(Mesh& t_mesh, AttributeSet& t_attribs, const st
 
     GPUBuffer outBuffer = GPUBuffer::create<float>(device, numPoints, VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
 
+    GPUBuffer inBufPermutations = GPUBuffer::create<int>(device, 512, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT);
+    inBufPermutations.write(perlinPermutations.data());
+
     auto descriptorsetLayout = GPUDescriptorSetLayout::Builder(device)
                                    .addBinding(0, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_COMPUTE_BIT, 1)
                                    .addBinding(1, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_COMPUTE_BIT, 1)
                                    .addBinding(2, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_COMPUTE_BIT, 1)
                                    .addBinding(3, VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, VK_SHADER_STAGE_COMPUTE_BIT, 1)
+                                   .addBinding(4, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_COMPUTE_BIT, 1)
                                    .build();
 
     GPUPipeline pipeline{device, "gpu/shaders/perlin.spv", *descriptorsetLayout};
@@ -70,8 +132,9 @@ void AttributeNoise::perlinNoise(Mesh& t_mesh, AttributeSet& t_attribs, const st
                             &inBufPosY,
                             &inBufPosZ,
                             &outBuffer,
+                            &inBufPermutations,
                         }};
-    task.run(numPoints);
+    task.run(numPoints / 256);
 
     auto attr = t_attribs.findOrCreate<float>(t_name, t_attrSize);
     const size_t attrSize = attr->attrSize();
--- src/core/nodes/attributes/attribute_noise.h
+++ src/core/nodes/attributes/attribute_noise.h
@@ -22,6 +22,9 @@ class AttributeNoise : public Node {
   private:
     std::shared_ptr<Mesh> compute(const size_t t_index,
                                   const std::vector<std::shared_ptr<Mesh>>& t_inputs) const override;
+
+  public:
+    static const std::array<int, 512> perlinPermutations;
 };
 
 } // namespace euclide
