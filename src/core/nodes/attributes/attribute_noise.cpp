d11b646e4b14339e00145ec2d5ffe9498e653c55
AnthonySTZ <anthonycandy2309@gmail.com>
2025-12-19 13:04:45 +0100

    feat(AttributeNoise): add it to nodes infos

--- src/core/nodes/attributes/attribute_noise.cpp
+++ src/core/nodes/attributes/attribute_noise.cpp
@@ -2,7 +2,42 @@
 
 namespace euclide {
 
-AttributNoise::AttributNoise() : Node(1, 1, "AttrNoise") {
+AttributeNoise::AttributeNoise() : Node(1, 1, "AttrNoise") {
+    auto kindField = std::make_shared<NodeField<int>>(0);
+    kindField->setMetadata(NodeFieldMetadata{
+        displayName : "Kind",
+        is_combo : true,
+        choices : std::move(std::vector<std::string>{"points", "primitives"})
+    });
+    addField("kind", kindField);
+
+    auto attributeField = std::make_shared<NodeField<std::string>>("Cd");
+    attributeField->setMetadata(NodeFieldMetadata{displayName : "Attribute Name"});
+    addField("attributeName", attributeField);
+
+    auto noiseField = std::make_shared<NodeField<int>>(0);
+    noiseField->setMetadata(NodeFieldMetadata{
+        displayName : "Noise",
+        is_combo : true,
+        choices : std::move(std::vector<std::string>{"Perlin"})
+    });
+    addField("noise", noiseField);
+}
+
+std::shared_ptr<Mesh> AttributeNoise::compute(const size_t t_index,
+                                              const std::vector<std::shared_ptr<Mesh>>& t_inputs) const {
+    if (t_inputs[0] == nullptr)
+        return std::make_shared<Mesh>();
+
+    auto output = std::make_shared<Mesh>(*t_inputs[0]);
+
+    perlinNoise(output->pointAttribs, "Cd", 3, 1, 1);
+
+    return output;
+}
+
+void AttributeNoise::perlinNoise(AttributeSet& t_attribs, const std::string& t_name, const int t_attrSize,
+                                 const int t_octaves, const float persistence) {
 }
 
 } // namespace euclide
--- src/core/nodes/attributes/attribute_noise.h
+++ src/core/nodes/attributes/attribute_noise.h
@@ -4,13 +4,14 @@
 
 namespace euclide {
 
-class AttributNoise : public Node {
+class AttributeNoise : public Node {
   public:
     enum Kind { POINTS = 0, PRIMITIVES = 1 };
 
-    AttributNoise();
+    AttributeNoise();
 
-    static void perlinNoise(AttributeSet& t_attribs, const std::string& t_name, const int t_attrSize);
+    static void perlinNoise(AttributeSet& t_attribs, const std::string& t_name, const int t_attrSize,
+                            const int t_octaves, const float persistence);
 
   private:
     std::shared_ptr<Mesh> compute(const size_t t_index,
