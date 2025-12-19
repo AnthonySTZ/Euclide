563cee394999d8e3093d73d0c07b6a0d52eea555
AnthonySTZ <anthonycandy2309@gmail.com>
2025-12-19 12:12:14 +0100

    feat(AttributeNoise): create files

---
+++ src/core/nodes/attributes/attribute_noise.cpp
@@ -0,0 +1,8 @@
+#include "attribute_noise.h"
+
+namespace euclide {
+
+AttributNoise::AttributNoise() : Node(1, 1, "AttrNoise") {
+}
+
+} // namespace euclide
---
+++ src/core/nodes/attributes/attribute_noise.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "nodes/node.h"
+
+namespace euclide {
+
+class AttributNoise : public Node {
+  public:
+    enum Kind { POINTS = 0, PRIMITIVES = 1 };
+
+    AttributNoise();
+
+    static void perlinNoise(AttributeSet& t_attribs, const std::string& t_name, const int t_attrSize);
+
+  private:
+    std::shared_ptr<Mesh> compute(const size_t t_index,
+                                  const std::vector<std::shared_ptr<Mesh>>& t_inputs) const override;
+};
+
+} // namespace euclide
