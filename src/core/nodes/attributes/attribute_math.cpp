fb27166aff31e7ee3db7b5715546384fec4a228e
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2025-12-24 11:55:35 +0100

    feat(AttributeMath): add fields

--- src/core/nodes/attributes/attribute_math.cpp
+++ src/core/nodes/attributes/attribute_math.cpp
@@ -13,22 +13,35 @@ AttributeMath::AttributeMath() : Node(1, 1, "AttrMath") {
     });
     addField("kind", kindField);
 
-    auto attributeField = std::make_shared<NodeField<std::string>>("Cd");
-    attributeField->setMetadata(NodeFieldMetadata{displayName : "Attribute Name"});
-    addField("attributeName", attributeField);
-
-    auto sizeField = std::make_shared<NodeField<int>>(0);
-    sizeField->setMetadata(NodeFieldMetadata{
-        displayName : "Attribute Size",
-        min : 0,
-        max : 4,
-        step : 1,
+    auto firstAttribField = std::make_shared<NodeField<std::string>>("");
+    firstAttribField->setMetadata(NodeFieldMetadata{
+        .displayName = "Attribute A",
     });
-    addField("size", sizeField);
+    addField("attrA", firstAttribField);
 
-    auto valueField = std::make_shared<Float4Field>(0.0f, 0.0f, 0.0f, 0.0f);
-    valueField->setMetadata(NodeFieldMetadata{displayName : "Attribute Value", step : 0.01f});
-    addField("value", valueField);
+    auto secondAttribField = std::make_shared<NodeField<std::string>>("");
+    secondAttribField->setMetadata(NodeFieldMetadata{
+        .displayName = "Attribute B",
+    });
+    addField("attrB", secondAttribField);
+
+    auto operationField = std::make_shared<NodeField<int>>(0);
+    operationField->setMetadata(NodeFieldMetadata{
+        displayName : "Operation",
+        is_combo : true,
+        choices : std::move(std::vector<std::string>{"Add", "Subtract", "Multiply", "Divide"})
+    });
+    addField("operation", operationField);
+
+    auto outputAttribField = std::make_shared<NodeField<std::string>>("");
+    outputAttribField->setMetadata(NodeFieldMetadata{
+        .displayName = "Output A",
+    });
+    addField("outputA", outputAttribField);
+}
+
+void AttributeMath::addAttribute(AttributeSet& t_attribs, const std::string& t_firstAttrib,
+                                 const std::string& t_secondAttrib, const std::string& t_resultAttrib) {
 }
 
 std::shared_ptr<Mesh> AttributeMath::compute(const size_t t_index, const std::vector<std::shared_ptr<Mesh>>& t_inputs) {
