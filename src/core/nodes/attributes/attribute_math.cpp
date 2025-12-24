d5315900e8776715f188f0f6664285eb35efdf05
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2025-12-24 12:42:00 +0100

    feat(AttributeMath): check attribute names

--- src/core/nodes/attributes/attribute_math.cpp
+++ src/core/nodes/attributes/attribute_math.cpp
@@ -35,13 +35,9 @@ AttributeMath::AttributeMath() : Node(1, 1, "AttrMath") {
 
     auto outputAttribField = std::make_shared<NodeField<std::string>>("");
     outputAttribField->setMetadata(NodeFieldMetadata{
-        .displayName = "Output A",
+        .displayName = "Output",
     });
-    addField("outputA", outputAttribField);
-}
-
-void AttributeMath::addAttribute(AttributeSet& t_attribs, const std::string& t_firstAttrib,
-                                 const std::string& t_secondAttrib, const std::string& t_resultAttrib) {
+    addField("output", outputAttribField);
 }
 
 std::shared_ptr<Mesh> AttributeMath::compute(const size_t t_index, const std::vector<std::shared_ptr<Mesh>>& t_inputs) {
@@ -50,7 +46,82 @@ std::shared_ptr<Mesh> AttributeMath::compute(const size_t t_index, const std::ve
 
     auto output = std::make_shared<Mesh>(*t_inputs[0]);
 
+    const std::string attrA = getField<NodeField<std::string>>("attrA")->getValue();
+    const std::string attrB = getField<NodeField<std::string>>("attrB")->getValue();
+    const std::string outputAttr = getField<NodeField<std::string>>("output")->getValue();
+
+    addAttributes(output->pointAttribs, attrA, attrB, outputAttr);
+
     return output;
+}
+
+void AttributeMath::addAttributes(AttributeSet& t_attribs, const std::string& t_firstAttrib,
+                                  const std::string& t_secondAttrib, const std::string& t_resultAttrib) {
+    auto attrAInfos = getAttrInfoByName(t_firstAttrib);
+    auto attrBInfos = getAttrInfoByName(t_secondAttrib);
+    auto outputInfos = getAttrInfoByName(t_resultAttrib);
+
+    if (!attrAInfos.has_value() || !attrBInfos.has_value() || !outputInfos.has_value())
+        return;
+
+    auto attrAInfosValue = attrAInfos.value();
+    auto attrBInfosValue = attrBInfos.value();
+    auto outputInfosValue = outputInfos.value();
+    bool isSingleComponent = attrAInfosValue.singleComponent;
+    if (isSingleComponent != attrBInfosValue.singleComponent || isSingleComponent != outputInfosValue.singleComponent)
+        return;
+
+    auto attrA = t_attribs.find(attrAInfosValue.name);
+    auto attrB = t_attribs.find(attrBInfosValue.name);
+    if (!attrA || !attrB)
+        return;
+
+    if (isSingleComponent) {
+        if (attrA->attrSize() <= attrAInfosValue.component || attrB->attrSize() <= attrBInfosValue.component)
+            return;
+    }
+
+    auto attrOut = t_attribs.findOrCreate<float>(outputInfosValue.name, attrA->attrSize());
+    if (isSingleComponent && attrOut->attrSize() <= outputInfosValue.component)
+        return;
+
+    if (!isSingleComponent) {
+        if (attrA->attrSize() != attrB->attrSize() || attrB->attrSize() != attrOut->attrSize())
+            return;
+    }
+}
+
+std::optional<AttributeMath::AttributeInfo> AttributeMath::getAttrInfoByName(const std::string& t_name) {
+    if (t_name.empty())
+        return std::nullopt;
+
+    AttributeInfo infos{};
+
+    size_t nameLength = t_name.length();
+    for (size_t i = 0; i < nameLength; ++i) {
+        if (t_name[i] == '.') {
+            if (i != nameLength - 2)
+                return std::nullopt;
+            const char component = t_name[i + 1];
+            if (component == 'x')
+                infos.component = 0;
+            else if (component == 'y')
+                infos.component = 1;
+            else if (component == 'z')
+                infos.component = 2;
+            else if (component == 'w')
+                infos.component = 3;
+            else
+                return std::nullopt;
+
+            infos.name = t_name.substr(0, i - 1);
+            infos.singleComponent = true;
+            return infos;
+        }
+    }
+
+    infos.name = t_name;
+    return infos;
 }
 
 } // namespace euclide
--- src/core/nodes/attributes/attribute_math.h
+++ src/core/nodes/attributes/attribute_math.h
@@ -1,20 +1,28 @@
 #pragma once
 
 #include "nodes/node.h"
+#include <optional>
 
 namespace euclide {
 
 class AttributeMath : public Node {
   public:
     enum Kind { POINTS = 0, PRIMITIVES = 1 };
+    struct AttributeInfo {
+        std::string name = "";
+        bool singleComponent = false;
+        int component = 0;
+    };
 
     AttributeMath();
 
-    static void addAttribute(AttributeSet& t_attribs, const std::string& t_firstAttrib,
-                             const std::string& t_secondAttrib, const std::string& t_resultAttrib);
+    static void addAttributes(AttributeSet& t_attribs, const std::string& t_firstAttrib,
+                              const std::string& t_secondAttrib, const std::string& t_resultAttrib);
 
   private:
     std::shared_ptr<Mesh> compute(const size_t t_index, const std::vector<std::shared_ptr<Mesh>>& t_inputs) override;
+
+    static std::optional<AttributeMath::AttributeInfo> getAttrInfoByName(const std::string& t_name);
 };
 
 } // namespace euclide
