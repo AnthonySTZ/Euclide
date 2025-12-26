f79f5f18109c3552b5392951f3bf2c2efa9a6f9d
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2025-12-26 01:09:19 +0100

    feat(Sax): add tokenizer and parser methods

--- src/core/sax/parser.h
+++ src/core/sax/parser.h
@@ -1,19 +1,68 @@
 #pragma once
 
+#include <iostream>
+
 #include "tokenizer.h"
 
 namespace euclide {
 
-struct AST {};
+enum LiteralType { NumericLiteral, StringLiteral };
+
+struct Literal {
+    LiteralType type;
+    std::string value;
+};
+
+/// @brief Abstract Syntax Tree defining a Program
+struct AST {
+    Literal body;
+};
 
 class Parser {
   public:
     Parser() = default;
 
-    AST parse(const std::string& t_text);
+    /// @brief Parse a given text to an Abstract Syntax Tree
+    /// @param t_text The script text to parse
+    /// @return The AST constructed from the given text
+    inline AST parse(const std::string& t_text) {
+        m_tokenizer.initialize(t_text);
+        m_nextToken = m_tokenizer.getNextToken();
+
+        return program();
+    };
 
   private:
-    std::string m_text;
+    inline AST program() { return AST{literal()}; }
+
+    inline Literal literal() {
+        switch (m_nextToken.type) {
+        case TokenType::Number:
+            return Literal{LiteralType::NumericLiteral, m_nextToken.value};
+
+        case TokenType::String:
+            return Literal{LiteralType::StringLiteral, m_nextToken.value};
+
+        default:
+            break;
+        }
+    }
+
+    inline Token consume(const TokenType t_tokenType) {
+        const Token token = m_nextToken;
+        if (token.type == TokenType::Undefined)
+            throw std::runtime_error("Unexpected end of input !");
+
+        if (token.type != t_tokenType)
+            throw std::runtime_error("Unexpected token !");
+
+        m_nextToken = m_tokenizer.getNextToken();
+        return token;
+    }
+
+  private:
+    Tokenizer m_tokenizer{};
+    Token m_nextToken{};
 };
 
 } // namespace euclide
--- src/core/sax/tokenizer.h
+++ src/core/sax/tokenizer.h
@@ -4,6 +4,61 @@
 
 namespace euclide {
 
-class Tokenizer {};
+enum TokenType { Number, String, Undefined };
+
+struct Token {
+    TokenType type = TokenType::Undefined;
+    std::string value{};
+};
+
+class Tokenizer {
+  public:
+    /// @brief Initialize Tokenizer to be used again with another text
+    /// @param t_text
+    void initialize(const std::string& t_text) {
+        m_text = t_text;
+        m_cursor = 0;
+    }
+
+    inline Token getNextToken() {
+        if (!hasMoreTokens())
+            return {
+                TokenType::Undefined,
+                "",
+            };
+
+        // Numbers
+        if (isNum(m_text[m_cursor])) {
+            const size_t start = m_cursor;
+            while (isNum(m_text[m_cursor]) && hasMoreTokens()) {
+                m_cursor++;
+            }
+            return {
+                TokenType::Number,
+                m_text.substr(start, m_cursor),
+            };
+        }
+
+        // String
+        if (m_text[m_cursor] == '"') {
+            const size_t start = m_cursor;
+            while (m_text[m_cursor] != '"' && hasMoreTokens()) {
+                m_cursor++;
+            }
+            return {
+                TokenType::String,
+                m_text.substr(start + 1, m_cursor - 1),
+            };
+        }
+    };
+
+  private:
+    inline bool [[nodiscard]] hasMoreTokens() const noexcept { return m_cursor < m_text.length(); }
+    inline bool [[nodiscard]] isNum(const char t_letter) const noexcept { return t_letter >= '0' && t_letter <= '9'; }
+
+  private:
+    std::string m_text{};
+    size_t m_cursor = 0;
+};
 
 } // namespace euclide
