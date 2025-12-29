4e49a79b4461c8c37a0c26278ac0e01d5a379d8e
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2025-12-29 18:08:50 +0100

    feat(Evaluator): add binary op

--- src/core/axia/evaluator.h
+++ src/core/axia/evaluator.h
@@ -20,6 +20,20 @@ struct AxiaEvaluator : ASTVisitor {
     Value visit(const StringLiteral& t_node) override { return t_node.value; }
     Value visit(const NumericLiteral& t_node) override { return t_node.value; }
     Value visit(const BinaryOp& t_node) override { // TODO:
+        const float lhs = std::get<float>(t_node.left->accept(*this));
+        const float rhs = std::get<float>(t_node.right->accept(*this));
+        switch (t_node.type) {
+        case NodeType::AddOp:
+            return lhs + rhs;
+        case NodeType::SubOp:
+            return lhs - rhs;
+        case NodeType::MultOp:
+            return lhs * rhs;
+        case NodeType::DivOp:
+            return lhs / rhs;
+        default:
+            break;
+        }
         return 0.0f;
     }
     Value visit(const Identifier& t_node) override {
