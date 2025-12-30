15eccc29299ded588117119cf9dbc8980ccd36a9
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2025-12-30 17:30:17 +0100

    bench(AXIA): add bench for axia

---
+++ bench/bench_axia.cpp
@@ -0,0 +1,23 @@
+#include <benchmark/benchmark.h>
+
+#include "nodes/primitive/grid.h"
+#include "nodes/attributes/attribute_noise.h"
+#include "nodes/attributes/attribute_axia.h"
+
+static void BM_AXIAGrid1000x1000(benchmark::State& state) { // 33.7 ms - laptop
+    euclide::GridSettings gridSettings{.divisions = {1000, 1000}};
+    euclide::Mesh mesh;
+    euclide::Grid::createGrid(mesh, gridSettings);
+    euclide::PerlinNoise::PerlinSettings perlinSettings{.octaves = 6, .frequency = 1.0};
+    euclide::PerlinNoise perlin{};
+    perlin.applyToMesh(mesh, mesh.pointAttribs, "height", 1, perlinSettings);
+
+    const std::string script = "v@P.y = f@height;";
+
+    for (auto _ : state) {
+        euclide::AttributeAXIA::computeAXIA(mesh.pointAttribs, script);
+        benchmark::DoNotOptimize(mesh);
+    }
+}
+
+BENCHMARK(BM_AXIAGrid1000x1000)->Unit(benchmark::kMillisecond);
