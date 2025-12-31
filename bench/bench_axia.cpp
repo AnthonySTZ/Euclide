18999eac9d6938e842c51887db44c47381b21d98
BloooddCooll <33907775+BloooddCooll@users.noreply.github.com>
2025-12-31 15:17:54 +0100

    bench(axia): add more complex script

--- bench/bench_axia.cpp
+++ bench/bench_axia.cpp
@@ -4,7 +4,7 @@
 #include "nodes/attributes/attribute_noise.h"
 #include "nodes/attributes/attribute_axia.h"
 
-static void BM_AXIAGrid1000x1000(benchmark::State& state) { // 40 ms - laptop
+static void BM_AXIAGrid1000x1000(benchmark::State& state) { // 300 ms - laptop
     euclide::GridSettings gridSettings{.divisions = {1000, 1000}};
     euclide::Mesh mesh;
     euclide::Grid::createGrid(mesh, gridSettings);
@@ -12,7 +12,9 @@ static void BM_AXIAGrid1000x1000(benchmark::State& state) { // 40 ms - laptop
     euclide::PerlinNoise perlin{};
     perlin.applyToMesh(mesh, mesh.pointAttribs, "height", 1, perlinSettings);
 
-    const std::string script = "v@P.y = f@height;";
+    const std::string script = "float amp = 0.5;"
+                               "float noise = f@height * amp;"
+                               "v@P.y = noise;";
 
     for (auto _ : state) {
         euclide::AttributeAXIA::computeAXIA(mesh.pointAttribs, script);
@@ -20,4 +22,4 @@ static void BM_AXIAGrid1000x1000(benchmark::State& state) { // 40 ms - laptop
     }
 }
 
-BENCHMARK(BM_AXIAGrid1000x1000)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_AXIAGrid1000x1000)->Iterations(50)->Unit(benchmark::kMillisecond);
